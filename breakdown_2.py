059255112283de9133e539b3ee239899c5b88c21
WillCPP <WillCPP@users.noreply.github.com>
2021-03-22 12:59:16 -0400

    Update breakdown_2.py

--- breakdown_2.py
+++ breakdown_2.py
@@ -3,6 +3,7 @@ from os.path import isfile, join, split
 from html.parser import HTMLParser
 import string
 import numpy as np
+import multiprocessing
 
 def pad_list(l):
     s = len(l)
@@ -26,66 +27,138 @@ class LineHTMLParser(HTMLParser):
             self.data.append(0)
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        # print("Encountered some data  :", data)
         if len(self.data) < 200 and data[0] != '(' and data[0] != '{':
             s = data.split(' ')
             for w in s[:200 - len(self.data)]:
                 self.data.append(1)
-            print(f'DONE| {len(self.data)}')
+            # print(f'DONE| {len(self.data)}')
 
 path = 'wikipediaFiles/output/'
-files = [f for f in listdir(path) if isfile(join(path, f))]
 
-arr_d = np.empty(shape=(0,200), dtype=int)
-arr_c = np.empty(shape=(0,1), dtype=int)
+# ==================================================
 
-i = 0
-count = 0
-for f in files:
-    atStart = True
-    atEnd = False
-    classList = []
-    with open(path+f, encoding='utf-8', errors='ignore') as file:
-        line = file.readline()
-        while line:
-            parser = LineHTMLParser()
-            # Do stuff
-            while 'mw-headline' not in line and atStart:
-                line = file.readline()
-                if 'mw-headline' in line:
-                    atStart = False
 
-            if 'id="Notes"' in line:
-                atEnd = True
-
-            if not atEnd:
-                classification = 1
-            else:
-                classification = 0
-
-            parser.feed(line)
-            classList.append(classification)
-            
-            if len(parser.data) == 200:
-                print('FULL')
-                print(parser.data)
-            pad_list(parser.data)
-            print(f'LEN: {len(parser.data)}')
-
-            # arr_d = np.append(arr_d, [parser.data])
-            arr_d = np.vstack((arr_d, parser.data))
-
+def process(files, i):
+    d = np.empty(shape=(0,200), dtype=int)
+    c = np.array([], dtype=int)
+    for f in files:
+        atStart = True
+        atEnd = False
+        classList = []
+        with open(path+f, encoding='utf-8', errors='ignore') as file:
             line = file.readline()
-            count += 1
-    
-    print(arr_d)
+            while line:
+                parser = LineHTMLParser()
+                while 'mw-headline' not in line and atStart:
+                    line = file.readline()
+                    if 'mw-headline' in line:
+                        atStart = False
+                if 'id="Notes"' in line:
+                    atEnd = True
+                if not atEnd:
+                    classification = 1
+                else:
+                    classification = 0
+                parser.feed(line)
+                classList.append(classification)
+                pad_list(parser.data)
+                d = np.vstack((d, parser.data))
+                line = file.readline()
+        c = np.append(c, np.array(classList, dtype=int))
+    np.save(f'dataset/data/{i}_data', d)
+    np.save(f'dataset/labels/{i}_labels', c)
 
-    np.save('r', arr_d)
-    np.savetxt('r.csv', arr_d, fmt='%d', delimiter=',')
+def run():
     
-    # print(classList)
-    print(count)
-    i += 1
-    if i > 0: # Early break for testing
-        break
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    print(len(files))
 
+    f0 = files[:739-1]
+    f1 = files[739:(2*739-1)]
+    f2 = files[(2*739):(3*739-1)]
+    f3 = files[(3*739):(4*739-1)]
+    f4 = files[(4*739):(5*739-1)]
+    f5 = files[(5*739):(6*739-1)]
+    f6 = files[(6*739):(7*739-1)]
+    f7 = files[(7*739):]
+
+    t0 = multiprocessing.Process(target=process, name=f'=process_0', args=(f0, 0,))
+    t0.start()
+    t1 = multiprocessing.Process(target=process, name=f'=process_1', args=(f1, 1,))
+    t1.start()
+    t2 = multiprocessing.Process(target=process, name=f'=process_2', args=(f2, 2,))
+    t2.start()
+    t3 = multiprocessing.Process(target=process, name=f'=process_3', args=(f3, 3,))
+    t3.start()
+    t4 = multiprocessing.Process(target=process, name=f'=process_4', args=(f4, 4,))
+    t4.start()
+    t5 = multiprocessing.Process(target=process, name=f'=process_5', args=(f5, 5,))
+    t5.start()
+    t6 = multiprocessing.Process(target=process, name=f'=process_6', args=(f6, 6,))
+    t6.start()
+    t7 = multiprocessing.Process(target=process, name=f'=process_7', args=(f7, 7,))
+    t7.start()
+
+if __name__ == '__main__':
+    run()
+# ==================================================
+
+
+
+# arr_d = np.empty(shape=(0,200), dtype=int)
+# # arr_c = np.empty(shape=(0,), dtype=int)
+# arr_c = np.array([], dtype=int)
+
+# i = 0
+# # count = 0
+# for f in files:
+#     atStart = True
+#     atEnd = False
+#     classList = []
+#     with open(path+f, encoding='utf-8', errors='ignore') as file:
+#         line = file.readline()
+#         while line:
+#             parser = LineHTMLParser()
+#             # Do stuff
+#             while 'mw-headline' not in line and atStart:
+#                 line = file.readline()
+#                 if 'mw-headline' in line:
+#                     atStart = False
+
+#             if 'id="Notes"' in line:
+#                 atEnd = True
+
+#             if not atEnd:
+#                 classification = 1
+#             else:
+#                 classification = 0
+
+#             parser.feed(line)
+#             classList.append(classification)
+            
+#             # if len(parser.data) == 200:
+#             #     print('FULL')
+#             #     print(parser.data)
+#             pad_list(parser.data)
+#             # print(f'LEN: {len(parser.data)}')
+
+#             arr_d = np.vstack((arr_d, parser.data))
+
+#             line = file.readline()
+#             # count += 1
+    
+#     # print(arr_d)
+#     # print(arr_d.shape)
+#     arr_c = np.append(arr_c, np.array(classList, dtype=int))
+#     # print(arr_c)
+#     # print(arr_c.shape)
+
+#     # np.save('r', arr_d)
+#     # np.savetxt('r.csv', arr_d, fmt='%d', delimiter=',')
+    
+#     i += 1
+#     if i % 10 == 0:
+#         print(f'FILES PROCESSED: {i}')
+#     # if i > 0: # Early break for testing
+#     #     break
