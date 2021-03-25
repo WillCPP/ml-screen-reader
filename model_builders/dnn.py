29b27e3129385bf77f4f14cb20254ec9de8eafa5
WillCPP <WillCPP@users.noreply.github.com>
2021-03-25 08:25:14 +0000

    In progress

--- model_builders/dnn.py
+++ model_builders/dnn.py
@@ -5,18 +5,59 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Conv1D, GlobalMaxPooling1D, Dense
 from tensorflow.keras.models import Sequential
+import argparse
+import os
 
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
 
-    # ==================================================
-    ds_data = np.load('dataset3/data.npy', allow_pickle=True)
-    ds_labels = np.load('dataset3/labels.npy', allow_pickle=True)
-    print(ds_data.shape)
-    print(ds_labels.shape)
-    x_train, x_val, y_train, y_val = train_test_split(ds_data, ds_labels, train_size=0.75)
+    # ================================================== AWS
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--learning-rate', type=float, default=0.01)
+    parser.add_argument('--batch-size', type=int, default=128)
+    parser.add_argument('--gpu-count', type=int, default=os.environ['SM_NUM_GPUS'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--training', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--validation', type=str, default=os.environ['SM_CHANNEL_VALIDATION'])
+    # ================================================== AWS
+
+#     # ================================================== Local
+#     parser.add_argument('--epochs', type=int, default=10)
+#     parser.add_argument('--learning-rate', type=float, default=0.01)
+#     parser.add_argument('--batch-size', type=int, default=128)
+#     parser.add_argument('--gpu-count', type=int, default=0)
+#     parser.add_argument('--model-dir', type=str, default='model')
+#     parser.add_argument('--training', type=str, default='data')
+#     parser.add_argument('--validation', type=str, default='data')
+#     # ================================================== Local
+    
+    args, _ = parser.parse_known_args()
+    
+    epochs     = args.epochs
+    lr         = args.learning_rate
+    batch_size = args.batch_size
+    gpu_count  = args.gpu_count
+    model_dir  = args.model_dir
+    training_dir   = args.training
+    validation_dir = args.validation
+
+#     # ==================================================
+#     ds_data = np.load('dataset3/data.npy', allow_pickle=True)
+#     ds_labels = np.load('dataset3/labels.npy', allow_pickle=True)
+#     print(ds_data.shape)
+#     print(ds_labels.shape)
+#     x_train, x_val, y_train, y_val = train_test_split(ds_data, ds_labels, train_size=0.75)
+#     x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+#     x_val = x_val.reshape(x_val.shape[0], x_val.shape[1], 1)
+#     # ==================================================
+
+    x_train = np.load(os.path.join(training_dir, 'training.npz'))['image']
+    y_train = np.load(os.path.join(training_dir, 'training.npz'))['label']
+    x_val  = np.load(os.path.join(validation_dir, 'validation.npz'))['image']
+    y_val  = np.load(os.path.join(validation_dir, 'validation.npz'))['label']
     x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
     x_val = x_val.reshape(x_val.shape[0], x_val.shape[1], 1)
-    # ==================================================
 
     print(len(x_train), "Training sequences")
     print(len(x_val), "Validation sequences")
