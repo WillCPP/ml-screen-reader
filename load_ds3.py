b5970f0c6c24a54cd1dc65c8b5aa86d0babccced
WillCPP <WillCPP@users.noreply.github.com>
2021-03-30 15:23:31 -0400

    Update load_ds3.py

--- load_ds3.py
+++ load_ds3.py
@@ -7,11 +7,21 @@ print(ds_labels.shape)
 
 count_0 = 0
 count_1 = 1
+index_0 = -1
 for i in ds_labels:
     if i == 0.0:
         count_0 += 1
+        if index_0 == -1:
+            index_0 = count_0 + count_1 - 1
     if i == 1.0:
         count_1 += 1
 
 print(f'0: {count_0}')
 print(f'1: {count_1}')
+print(f'i: {index_0}')
+
+print(ds_data[821])
+print(ds_labels[821])
+
+print(ds_data[0])
+print(ds_labels[0])
